e24526f1bdfdd768d930d1ea2981905c75013fbc
EC2 Default User <ec2-user@ip-172-31-53-49.ec2.internal>
2021-01-20 11:56:00 +0000

    modify comments in list method

--- todos/list.py
+++ todos/list.py
@@ -12,7 +12,7 @@ def list(event, context):
     # fetch all todos from the database
     result = table.scan()
 
-    # create a response for list method
+    # create a response
     response = {
         "statusCode": 200,
         "body": json.dumps(result['Items'], cls=decimalencoder.DecimalEncoder)
